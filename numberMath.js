4b1e3ed185d3eb84d322e5b206c3c813588f01e2
Nilesh Kumar <nilesh5india.nk@gmail.com>
2024-06-04 17:03:05 +0530

    numberMath

---
+++ numberMath.js
@@ -0,0 +1,34 @@
+//----------number----------
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2))
+
+const otherNumber = 124.845
+// console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//---------math------
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.abs(5));
+// console.log(Math.round(4.2));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+// console.log(Math.min(4, 5, 3, 7, 6));
+// console.log(Math.max(4, 5, 3, 7, 6));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+console.log(Math.random()*10 +1);

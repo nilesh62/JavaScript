92a824d858ff0e78c974b411b3598ba76a6ed6d1
Nilesh Kumar <nilesh5india.nk@gmail.com>
2024-06-07 00:16:31 +0530

    object3.js done

---
+++ object3.js
@@ -0,0 +1,25 @@
+// Object and API
+
+const course = {
+    coursename: "javascript",
+    price: "999",
+    courseInstructor: "Nilesh"
+}
+//course.courseInstructor
+
+const{courseInstructor: instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
+
+//json
+//  {
+
+//  }
+// json end
+
+// {
+//     "name": "Nilesh",
+//     "coursename": "Javascript",
+//     "price": "free"
+
+// }

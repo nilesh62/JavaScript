84d971af75b8fb72d07863d175edc5627725cf5e
Nilesh Kumar <nilesh5india.nk@gmail.com>
2024-06-07 22:23:35 +0530

    scope2.js done

---
+++ scope2.js
@@ -0,0 +1,42 @@
+//scope
+
+function one (){
+    const username = "Nilesh"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+ one()
+
+ if (true) {
+    const username = "Nilesh"
+    if (username === "Nilesh") {
+        const website = " youtube"
+        // console.log(username + website);
+
+        // console.log(website);
+        // console.log(username);
+    }
+    
+ 
+
+console.log(addone(5));
+
+ function addone(num) {       // this is write way
+    return num + 1    
+ 
+ }
+// console.log(addTwo(5));
+ const addTwo = function(num2) {    // this is wrong way.
+    return num2 + 1
+    
+ }
+
+    
+ }
+ 
+ 

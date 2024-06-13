26b631c33b5806a90a05bdffc57e61359d149868
Nilesh Kumar <nilesh5india.nk@gmail.com>
2024-06-13 16:00:23 +0530

    switch.js done

---
+++ switch.js
@@ -0,0 +1,50 @@
+// switch
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+        
+// const month = 2
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+
+
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+

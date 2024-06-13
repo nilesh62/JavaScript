cc01a597bd736cebd9ef37755967b266fc038412
Nilesh Kumar <nilesh5india.nk@gmail.com>
2024-06-13 17:32:57 +0530

    truthy.js done

---
+++ truthy.js
@@ -0,0 +1,54 @@
+// const userEmail = "nilesh@gmail.com"
+
+// if (userEmail) {
+//     console.log("got user email");
+// } else {
+//     console.log("dont have user email");
+// }
+
+// const userEmail = ""
+
+// if (userEmail) {
+//     console.log("got user email");
+// } else {
+//     console.log("dont have user email");
+// }
+
+// const userEmail = []
+
+// if (userEmail) {
+//     console.log("got user email");
+// } else {
+//     console.log("dont have user email");
+// }
+
+// falsy values -- false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// Truthy values --  "0", 'false', "space", [], {}, function(){}, 
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("object is empty");
+// }
+
+// Nullish coalescing operator(??) null and undefined
+
+// let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 15
+
+// console.log(val1);
+
+// Terniary Operator 
+
+// condition ? true : false
+
+const teaPrice = 100
+// teaPrice <= 80 ? console.log("less then 80") : console.log("more then 80");

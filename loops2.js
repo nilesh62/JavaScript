598759e7fb5df3675830b918834f10662a620efb
Nilesh Kumar <nilesh5india.nk@gmail.com>
2024-06-14 17:42:31 +0530

    do while loops done

---
+++ loops2.js
@@ -0,0 +1,43 @@
+// loops
+
+// while loops
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let index = 1
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let index = 2
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArray = [ "flash", "batman", "superman", "spiderman" ]
+
+// let arr = 0
+
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+// do while loops
+
+// let score = 1
+// do {
+//     console.log(`score is ${score}`);
+//     score++
+// } while (score <= 10);
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
